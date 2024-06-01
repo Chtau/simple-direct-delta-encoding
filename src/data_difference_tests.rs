162836d8ece141a989b3ad35b6ae34ff31e55895
Christoph Taucher <taucher.ch@gmail.com>
2024-06-01 14:22:02 +0200

    integrate patch creation and apply them

---
+++ src/data_difference_tests.rs
@@ -0,0 +1,211 @@
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn diff_replace_action() {
+        let data_old = "Test1".as_bytes();
+        let data_new = "Test2".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff.last().unwrap().to_bytes(), vec![b'r', b':', 4, b'-', 1, b'2']);
+    }
+    
+    #[test]
+    fn diff_insert_action() {
+        let data_old = "Test".as_bytes();
+        let data_new = "Test2".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff.last().unwrap().to_bytes(), vec![b'i', b':', 4, b'-', 1, b'2']);
+    }
+        
+    #[test]
+    fn diff_delete_action() {
+        let data_old = "Test2".as_bytes();
+        let data_new = "Test".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff.last().unwrap().to_bytes(), vec![b'd', b':', 4, b'-', 1]);
+    }
+    
+    #[test]
+    fn diff_replace_action_with_same_inbetween() {
+        let data_old = "Test1THELLO".as_bytes();
+        let data_new = "Test2Thello".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff.last().unwrap().to_bytes(), vec![b'r', b':', 4, b'-', 7, b'2', b'T', b'h', b'e', b'l', b'l', b'o']);
+    }
+
+    #[test]
+    fn diff_large_data_sets() {
+        let data_old = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let data_new = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff.last().unwrap().to_bytes(), vec![b'r', b':', 9, b'-', 1, 11]);
+    }
+
+    #[test]
+    fn diff_multiple_actions() {
+        let data_old = vec![1, 2, 3, 4, 5];
+        let data_new = vec![1, 2, 6, 7, 8, 9];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        println!("{:?} String: {:?}", diff, std::str::from_utf8(&diff.last().unwrap().value).unwrap());
+        assert_eq!(diff[0].to_bytes(), vec![b'r', b':', 2, b'-', 3, 6, 7, 8]);
+        assert_eq!(diff[1].to_bytes(), vec![b'i', b':', 5, b'-', 1, 9]);
+    }
+
+    #[test]
+    fn diff_no_changes() {
+        let data_old = vec![1, 2, 3, 4, 5];
+        let data_new = vec![1, 2, 3, 4, 5];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        assert_eq!(diff.len(), 0);
+    }
+
+    #[test]
+    fn diff_range_length_u16() {
+        let data_old = vec![0; 300];
+        let data_new = vec![3; 300];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        assert_eq!(diff.last().unwrap().to_bytes()[..20], [vec![b'r', b':', 0, b'-', b's', 1, 44], vec![3; 300]].concat()[..20]);
+    }
+
+    #[test]
+    fn diff_range_length_u32() {
+        let data_old = vec![0; 65_536];
+        let data_new = vec![3; 65_536];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        assert_eq!(diff.last().unwrap().to_bytes()[..20], [vec![b'r', b':', 0, b'-', b'i', 0, 1, 0, 0], vec![3; 65_536]].concat()[..20]);
+    }
+
+    #[test]
+    #[ignore]
+    fn diff_range_length_u64() {
+        let data_old = vec![0; 4_294_967_296];
+        let data_new = vec![3; 4_294_967_296];
+        let diff = DataDifference::diff(&data_old, &data_new);
+        assert_eq!(diff.last().unwrap().to_bytes()[..20], [vec![b'r', b':', 0, b'-', b'l', 0, 0, 0, 1, 0, 0, 0, 0], vec![3; 4_294_967_296]].concat()[..20]);
+    }
+
+    #[test]
+    fn from_bytes_replace_action() {
+        let bytes = vec![b'r', b':', 4, b'-', 1, b'2'];
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(4, 1));
+        assert_eq!(difference.value, vec![b'2']);
+        assert!(!difference.is_open);
+    }
+
+    #[test]
+    fn from_bytes_insert_action() {
+        let bytes = vec![b'i', b':', 4, b'-', 1, b'2'];
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Insert);
+        assert_eq!(difference.range, Range::new(4, 1));
+        assert_eq!(difference.value, vec![b'2']);
+    }
+
+    #[test]
+    fn from_bytes_delete_action() {
+        let bytes = vec![b'd', b':', 4, b'-', 1];
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Delete);
+        assert_eq!(difference.range, Range::new(4, 1));
+        assert_eq!(difference.value, vec![]);
+    }
+
+    #[test]
+    fn from_bytes_replace_action_with_same_inbetween() {
+        let bytes = vec![b'r', b':', 4, b'-', 7, b'2', b'T', b'h', b'e', b'l', b'l', b'o'];
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(4, 7));
+        assert_eq!(difference.value, vec![b'2', b'T', b'h', b'e', b'l', b'l', b'o']);
+    }
+
+    #[test]
+    fn from_bytes_large_data_sets() {
+        let bytes = vec![b'r', b':', 9, b'-', 1, 11];
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(9, 1));
+        assert_eq!(difference.value, vec![11]);
+    }
+
+    #[test]
+    fn from_bytes_multiple_actions() {
+        let bytes1 = vec![b'r', b':', 2, b'-', 3, 6, 7, 8];
+        let difference1 = Difference::from_bytes(&bytes1);
+        assert_eq!(difference1.action, DifferenceAction::Replace);
+        assert_eq!(difference1.range, Range::new(2, 3));
+        assert_eq!(difference1.value, vec![6, 7, 8]);
+
+        let bytes2 = vec![b'i', b':', 5, b'-', 1, 9];
+        let difference2 = Difference::from_bytes(&bytes2);
+        assert_eq!(difference2.action, DifferenceAction::Insert);
+        assert_eq!(difference2.range, Range::new(5, 1));
+        assert_eq!(difference2.value, vec![9]);
+    }
+
+    #[test]
+    fn from_bytes_range_length_u16() {
+        let bytes = [vec![b'r', b':', 0, b'-', b's', 1, 44], vec![3; 300]].concat();
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(0, 300));
+        assert_eq!(difference.value, vec![3; 300]);
+    }
+
+    #[test]
+    fn from_bytes_range_length_u32() {
+        let bytes = [vec![b'r', b':', 0, b'-', b'i', 0, 1, 0, 0], vec![3; 65_536]].concat();
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(0, 65_536));
+        assert_eq!(difference.value, vec![3; 65_536]);
+    }
+
+    #[test]
+    #[ignore]
+    fn from_bytes_range_length_u64() {
+        let bytes = [vec![b'r', b':', 0, b'-', b'l', 0, 0, 0, 1, 0, 0, 0, 0], vec![3; 4_294_967_296]].concat();
+        let difference = Difference::from_bytes(&bytes);
+        assert_eq!(difference.action, DifferenceAction::Replace);
+        assert_eq!(difference.range, Range::new(0, 4_294_967_296));
+        assert_eq!(difference.value, vec![3; 4_294_967_296]);
+    }
+
+    #[test]
+    fn apply_diff_replace_action() {
+        let data_old = "Test1".as_bytes();
+        let data_new = "Test2".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        let data = data_old.to_owned();
+        let data = DataDifference::apply_diff(&data, &diff);
+        assert_eq!(data, data_new);
+    }
+    
+    #[test]
+    fn apply_diff_insert_action() {
+        let data_old = "Test".as_bytes();
+        let data_new = "Test2".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        let data = data_old.to_owned();
+        let data = DataDifference::apply_diff(&data, &diff);
+        assert_eq!(data, data_new);
+    }
+    
+    #[test]
+    fn apply_diff_delete_action() {
+        let data_old = "Test2".as_bytes();
+        let data_new = "Test".as_bytes();
+        let diff = DataDifference::diff(data_old, data_new);
+        let data = data_old.to_owned();
+        let data = DataDifference::apply_diff(&data, &diff);
+        assert_eq!(data, data_new);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,282 +1,85 @@
+mod data_difference;
 #[cfg(test)]
 mod tests;
+#[cfg(test)]
+mod data_difference_tests;
 
+use data_difference::*;
+use dispnet_hash::{DispnetHash, HashType};
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum DifferenceAction {
-    Replace,
-    Insert,
-    Delete,
+pub enum SDDEError {
+    CRC(String)
 }
 
-impl From<u8> for DifferenceAction {
-    fn from(value: u8) -> Self {
-        match value {
-            b'r' => DifferenceAction::Replace,
-            b'i' => DifferenceAction::Insert,
-            b'd' => DifferenceAction::Delete,
-            _ => panic!("Invalid difference action")
-        }
-    }
+pub struct SimpleDirectDeltaEncoding {
+    data: Vec<u8>,
+    crc: DispnetHash,
 }
-
-impl From<DifferenceAction> for u8 {
-    fn from(val: DifferenceAction) -> Self {
-        match val {
-            DifferenceAction::Replace => b'r',
-            DifferenceAction::Insert => b'i',
-            DifferenceAction::Delete => b'd',
-        }
-    }
-}
-
-/// range indicator start-length (in byte conversion a prefix is used for the usize type [implicit if no other short key matches it is default u8 value])
-#[derive(Debug, PartialEq)]
-pub struct Range {
-    pub start: usize,
-    pub length: usize,
-}
-
-impl Range {
-    pub fn new(start: usize, length: usize) -> Self {
-        Self {
-            start,
-            length,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub enum USizeType {
-    /// '' = byte/u8 [0-255]
-    U8,
-    /// s = short/u16 [0-65,535]
-    U16,
-    /// i = int/u32 [0-4,294,967,295]
-    U32,
-    /// l = long/u64 [0-18,446,744,073,709,551,615]
-    U64,
-}
-
-impl From<u8> for USizeType {
-    fn from(value: u8) -> Self {
-        match value {
-            b's' => USizeType::U16,
-            b'i' => USizeType::U32,
-            b'l' => USizeType::U64,
-            _ => USizeType::U8,
-        }
-    }
-}
-
-impl From<USizeType> for u8 {
-    fn from(val: USizeType) -> Self {
-        match val {
-            USizeType::U16 => b's',
-            USizeType::U32 => b'i',
-            USizeType::U64 => b'l',
-            _ => b'u',
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Difference {
-    pub action: DifferenceAction,
-    pub range: Range,
-    pub value: Vec<u8>,
-    pub is_open: bool,
-}
-
-impl Difference {
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let mut diff = Vec::new();
-        diff.push(self.action.into());
-        diff.push(b':');
-        diff.extend(Self::get_usize_type_to_bytes(self.range.start));
-        diff.push(b'-');
-        diff.extend(Self::get_usize_type_to_bytes(self.range.length));
-        if self.action != DifferenceAction::Delete {
-            diff.extend(self.value.to_owned());
-        }
-        diff
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        let action: DifferenceAction = bytes[0].into();
-        let range_start = Self::get_usize_type_from_bytes(&bytes[2..]);
-        let offset = 2 + range_start.1 + 1; // 2 bytes for action and range start, range start bytes count, 1 byte for range separator
-        let range_end = Self::get_usize_type_from_bytes(&bytes[offset..]);
-        let offset = offset + range_end.1;
-        let value = bytes[offset..].to_vec();
-        let is_open = false;
-        Self {
-            action,
-            range: Range::new(range_start.0, range_end.0),
-            value,
-            is_open,
-        }
-    }
-
-    fn get_usize_type_to_bytes(value: usize) -> Vec<u8> {
-        if value <= 255 {
-            (value as u8).to_be_bytes().to_vec()
-        } else if value <= 65_535 {
-            [vec![USizeType::U16.into()], (value as u16).to_be_bytes().to_vec()].concat()
-        } else if value <= 4_294_967_295 {
-            [vec![USizeType::U32.into()], (value as u32).to_be_bytes().to_vec()].concat()
-        } else {
-            [vec![USizeType::U64.into()], (value as u64).to_be_bytes().to_vec()].concat()
-        }
-    }
-
-    /// Returns the usize value and the bytes count of the usize type
-    fn get_usize_type_from_bytes(bytes: &[u8]) -> (usize, usize) {
-        match bytes[0].into() {
-            USizeType::U16 => {
-                let mut buffer = [0; 2];
-                buffer.copy_from_slice(&bytes[1..3]);
-                (u16::from_be_bytes(buffer) as usize, 3)
-            },
-            USizeType::U32 => {
-                let mut buffer = [0; 4];
-                buffer.copy_from_slice(&bytes[1..5]);
-                (u32::from_be_bytes(buffer) as usize, 5)
-            },
-            USizeType::U64 => {
-                let mut buffer = [0; 8];
-                buffer.copy_from_slice(&bytes[1..9]);
-                (u64::from_be_bytes(buffer) as usize, 9)
-            },
-            _ => (bytes[0] as usize, 1)
-        }
-    }
-}
-
-pub struct SimpleDirectDeltaEncoding { }
 
 impl SimpleDirectDeltaEncoding {
-    pub fn diff(old_data: &[u8], new_data: &[u8]) -> Vec<Difference> {
-        let mut differences: Vec<Difference> = Vec::new();
-        let mut same_count = 0;
-        let mut same_byte_buffer: Vec<u8> = Vec::new();
-        
-        for (i, b) in new_data.iter().enumerate() {
-            let old_b = old_data.get(i);
-            if let Some(old_byte) = old_b {
-                // replace action
-                if old_byte != b {
-
-                    if let Some(diff) = differences.last_mut() {
-                        if !diff.is_open {
-                            differences.push(Difference {
-                                action: DifferenceAction::Replace,
-                                range: Range::new(i, 1),
-                                value: vec![*b],
-                                is_open: true,
-                            });
-                        } else {
-                            // if we have a same byte buffer, we append it to the value
-                            if same_count > 0 {
-                                diff.value.extend(same_byte_buffer.clone());
-                                diff.range.length += same_count;
-                                same_byte_buffer.clear();
-                                same_count = 0;
-                            }
-                            diff.value.push(*b);
-                            diff.range.length += 1;
-                        }
-                    } else {
-                        differences.push(Difference {
-                            action: DifferenceAction::Replace,
-                            range: Range::new(i, 1),
-                            value: vec![*b],
-                            is_open: true,
-                        });
-                    }
-                } else {
-                    // old and current are the same
-                    // if we have a diff start index and we have differences
-
-                    if let Some(diff) = differences.last_mut() {
-                        if same_count > 1 {
-                            // if we have more than 1 same byte, we close the difference structure
-                            diff.is_open = false;
-
-                            same_count = 0;
-                            same_byte_buffer.clear();
-                        } else {
-                            same_count += 1;
-                            same_byte_buffer.push(*b);
-                        }
-                    }
-                }
-            } else {
-                // insert action
-                if let Some(diff) = differences.last_mut() {
-                    if !diff.is_open {
-                        differences.push(Difference {
-                            action: DifferenceAction::Insert,
-                            range: Range::new(i, 1),
-                            value: vec![*b],
-                            is_open: true,
-                        });
-                    } else {
-                        // if open and is insert, we append the value
-                        if diff.action == DifferenceAction::Insert {
-                            diff.value.push(*b);
-                            diff.range.length += 1;
-                        } else {
-                            // if open and is not insert, we close the difference and create a new insert
-                            same_count = 0;
-                            same_byte_buffer.clear();
-                            diff.is_open = false;
-                            differences.push(Difference {
-                                action: DifferenceAction::Insert,
-                                range: Range::new(i, 1),
-                                value: vec![*b],
-                                is_open: true,
-                            });
-                        }
-                    }
-                } else {
-                    differences.push(Difference {
-                        action: DifferenceAction::Insert,
-                        range: Range::new(i, 1),
-                        value: vec![*b],
-                        is_open: true,
-                    });
-                }
-            }
+    pub fn new(data: Vec<u8>) -> SimpleDirectDeltaEncoding {
+        let crc = DispnetHash::create(HashType::CRC, &data, None);
+        SimpleDirectDeltaEncoding {
+            data,
+            crc,
         }
-
-        // close remaining open differences
-        if let Some(diff) = differences.last_mut() {
-            diff.is_open = false;
-        }
-
-        // check for deletion on the end
-        if old_data.len() > new_data.len() {
-            differences.push(Difference {
-                action: DifferenceAction::Delete,
-                range: Range::new(new_data.len(), old_data.len() - new_data.len()),
-                value: vec![],
-                is_open: false,
-            });
-        }
-
-        differences
     }
 
-    pub fn apply_diff(data: &mut [u8], diff: &[Difference]) {
-        for d in diff {
-            if d.action == DifferenceAction::Replace {
-                data[d.range.start..(d.range.start + d.range.length)].copy_from_slice(&d.value);       
-            } else if d.action == DifferenceAction::Insert {
-                data[d.range.start..d.range.start].copy_from_slice(&d.value);
-            } else if d.action == DifferenceAction::Delete {
-                data[d.range.start..(d.range.start + d.range.length)].copy_from_slice(&[]);
-            }
+    /// Patch the data with the new data and return the diff data
+    /// 
+    /// 
+    /// The diff data is a byte array with the following format:
+    /// [CRC length, CRC value, Difference 1, Difference 2, ...]
+    /// The CRC length is a single byte that represents the length of the CRC value
+    /// The CRC value is the hash of the data before patching
+    /// The Difference is a struct that represents the difference between the old and new data
+    /// 
+    /// 
+    /// The Difference is a byte array with the following format:
+    /// [Action, Range start, Range length, Value]
+    /// The Action is a single byte that represents the action that should be taken
+    /// The Range start is a variable length byte array that represents the start index of the range
+    /// The Range length is a variable length byte array that represents the length of the range
+    /// The Value is a variable length byte array that represents the value that should be inserted
+    /// The Value is only present in the Replace and Insert actions
+    pub fn patch(&mut self, new_data: &[u8]) -> Vec<u8> {
+        let last_diff = DataDifference::diff(&self.data, new_data);
+        self.data = new_data.to_vec();
+        
+        let mut diff_data: Vec<u8> = vec![self.crc.digest_length as u8];
+        diff_data.extend(self.crc.digest_value.clone());
+        for diff in last_diff.iter() {
+            let bytes = diff.to_bytes();
+            diff_data.extend(Difference::get_usize_type_to_bytes(bytes.len()));
+            diff_data.extend(bytes);
         }
+        diff_data
+    }
+
+    pub fn apply_patch(&mut self, diff_data: &[u8]) -> Result<Vec<u8>, SDDEError> {
+        let crc_length = diff_data[0];
+        let crc_value = &diff_data[1..(1 + crc_length as usize)];
+        let diff_bytes = &diff_data[(1 + crc_length as usize)..];
+        let crc = DispnetHash::create(HashType::CRC, &self.data, None);
+        if crc.digest_value != crc_value {
+            return Err(SDDEError::CRC("CRC value does not match".to_owned()));
+        }
+
+        let mut diffs: Vec<Difference> = Vec::new();
+        let mut i = 0;
+        while i < diff_bytes.len() {
+            let diff_length = Difference::get_usize_type_from_bytes(&diff_bytes[i..]);
+            i += diff_length.1;
+            let bytes = &diff_bytes[i..(i + diff_length.0)];
+            let diff = Difference::from_bytes(bytes);
+            i += diff_length.0;
+            diffs.push(diff);
+        }
+        
+        let data = DataDifference::apply_diff(&self.data, &diffs);
+        self.data = data;
+        self.crc = crc;
+
+        Ok(self.data.clone())
     }
 }
